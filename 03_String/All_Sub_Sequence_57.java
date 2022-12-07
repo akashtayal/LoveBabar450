86d724285abc0ccc5b058c85c446fbc348290d5a
akash tayal <akashtayal999@gmail.com>
2022-12-07 21:45:10 +0530

    Adding All sub Sequence Code

---
+++ 03_String/All_Sub_Sequence_57.java
@@ -0,0 +1,20 @@
+package com.learning.lovebabar450.string;
+
+import java.util.ArrayList;
+
+public class All_Sub_Sequence_57 {
+	
+	public void subSequence(String inputString,String seq,ArrayList<String> list){
+	
+		if(inputString.length()==0) {
+			list.add(seq);
+			return ;
+		}
+		
+		//including character at index 0
+		subSequence(inputString.substring(1),seq+inputString.charAt(0),list);
+		//excluding character at index 0
+		subSequence(inputString.substring(1),seq,list);
+	}
+
+}
